c655978be7cf16f2140f092777affd00e51b7170
Rohit Bharti <rohitbharti279@gmail.com>
2024-01-07 12:14:48 +0530

    also reverse negative number

--- js/problem_7/main.js
+++ js/problem_7/main.js
@@ -3,17 +3,20 @@
  * @return {number}
  */
 var reverse = function(x) {
-    let reverse = 0;
-    while(x !== 0) {
-        let digit = x % 10;
-        reverse = reverse * 10 + digit;
-        x = Math.floor(x / 10);
-    }
+    // let reverse = 0;
+    // while(x !== 0) {
+    //     let digit = x % 10;
+    //     reverse = reverse * 10 + digit;
+    //     x = Math.floor(x / 10);
+    // }
 
-    return reverse;
+    // return reverse;
+
+    let reverse = parseInt(x.toString().split("").reverse().join(""));
+    return (x < 0) ? -reverse : reverse;
 };
 
 console.log(reverse(123))
 console.log(reverse(23234243))
-console.log(reverse(989832989823498123))
-console.log(reverse(123))
+console.log(reverse(-2989823498123))
+console.log(reverse(-123))
