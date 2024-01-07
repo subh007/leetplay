9c9cccad06d0154e6c5ec2670306aacb0fb8bdb3
Rohit Bharti <rohitbharti279@gmail.com>
2024-01-07 12:28:17 +0530

    Constraints

--- js/problem_7/main.js
+++ js/problem_7/main.js
@@ -2,7 +2,7 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
+var reverse = function (x) {
     // let reverse = 0;
     // while(x !== 0) {
     //     let digit = x % 10;
@@ -13,10 +13,17 @@ var reverse = function(x) {
     // return reverse;
 
     let reverse = parseInt(x.toString().split("").reverse().join(""));
-    return (x < 0) ? -reverse : reverse;
+
+    if (reverse > Math.pow(-2 , 31) && reverse < Math.pow(2 , 31) -1) {
+        // console.log(Math.pow(-2 , 31));
+        return (x < 0) ? -reverse : reverse;
+    }
+
+    else
+        return 0;
 };
 
 console.log(reverse(123))
-console.log(reverse(23234243))
-console.log(reverse(-2989823498123))
-console.log(reverse(-123))
+console.log(reverse(93234243999))
+// console.log(reverse(-2989823498123))
+// console.log(reverse(-123))
